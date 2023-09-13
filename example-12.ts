f350c594ac9c899235e7dbb0420bb008b8b6c754
Andrew Alsing <andrew32alsing@gmail.com>
2023-09-13 11:19:43 -0700

    completed challenges 10-14

--- example-12.ts
+++ example-12.ts
@@ -1,41 +1,44 @@
+// You are making a mapping application.
 
-// You are making a mapping application. 
-
-// Write an enum that defines the directions: 
+// Write an enum that defines the directions:
 // North, South, East, West
 // Use a string enum: https://www.typescriptlang.org/docs/handbook/enums.html#string-enums
 
 enum Direction {
-	
+  North = "North",
+  South = "South",
+  East = "East",
+  West = "West",
 }
 
 // Should having a heading property type Direction
 
 class MapPosition {
+  heading: Direction;
 
-	constructor() {
-		
-	}
+  constructor(startingDirection: Direction = Direction.North) {
+    this.heading = startingDirection;
+  }
 
-	// takes a new Direction as an argument
-	move(newDirection: Direction) {
-		// set the direction on your property 
-		// Print the new direction
-	}
+  // takes a new Direction as an argument
+  move(newDirection: Direction) {
+    // set the direction on your property
+    this.heading = newDirection;
+    // Print the new direction
+    console.log(this.heading);
+  }
 
-	// Should return a string and print:
-	// "you are heading <direction>"
-	describe(): string {
-		return ''
-	}
+  // Should return a string and print:
+  // "you are heading <direction>"
+  describe(): string {
+    return `You are heading ${this.heading}`;
+  }
 }
 
-const location = new MapPosition()
-console.log(location.describe())
+const location = new MapPosition();
+console.log(location.describe());
 // Might output:
 // "You are heading north"
-location.move(/* add a direction here */)
+location.move(Direction.North);
 
-
-
-export default MapPosition
+export default MapPosition;
